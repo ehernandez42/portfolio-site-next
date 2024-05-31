850386dfab58a05364ebaf713fd6f9f7f69c951b
Eleazar <eleazarfhernandez@gmail.com>
2024-05-31 18:59:18 -0500

    API key isn't being read. at least we know wat happened! haha

--- portfolio-site-next/src/app/contact/actions/email-action.ts
+++ portfolio-site-next/src/app/contact/actions/email-action.ts
@@ -4,7 +4,7 @@ import {Resend} from "resend";
 import React from "react";
 
 
-const resend = new Resend(`${process.env.RESEND_API_KEY}`);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 export async function sendEmail(prevState: any | undefined, formData: FormData) {
