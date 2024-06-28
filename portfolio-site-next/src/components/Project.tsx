b7f7251805423d8a660318bdbbe77dacac2e87c0
Eleazar <eleazarfhernandez@gmail.com>
2024-06-28 10:37:43 -0500

    fixing key issue in my map of hashTags

--- portfolio-site-next/src/components/Project.tsx
+++ portfolio-site-next/src/components/Project.tsx
@@ -29,9 +29,9 @@ const Project = ({projects}: props) => {
                 </a>
             </div>
             <div className="px-6 pt-4 pb-2">
-                {hashTags.map((tag) => {
+                {hashTags.map((tag, index) => {
                     return (
-                        <span
+                        <span key={index}
                             className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                                 #{tag}
                             </span>
